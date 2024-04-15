834d2f11d4fa462e725e97b9ab24bf18ab4bde7a
Alireza <42074597+arbor0711@users.noreply.github.com>
2024-04-15 09:31:11 +0200

    Create navbar layout.

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import "./navbar.css";
+import logo from "../../assets/images/logo.png";
+
+const Navbar = () => {
+  return (
+    <nav className="container">
+      <img src={logo} alt="Logo image for Optic Choice company" />
+      <ul>
+        <li>Home</li>
+        <li>Glasses</li>
+        <li>About Us</li>
+        <li>Testimonials</li>
+        <li>
+          <button className="btn">Contact Us</button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
